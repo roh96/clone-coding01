e4b728c81c25dd6454e4472d61ea9f50b0c01c90
roh96 <74894952+roh96@users.noreply.github.com>
2023-01-30 03:29:28 +0900

    add first intro_sub01

---
+++ inc.js
@@ -0,0 +1,8 @@
+//const topBtn = document.querySelector(".up-tool");
+// window.addEventListener("click", contents);
+// function contents() {
+//   window.scrollTo({ top: 0, behavior: "smooth" });
+// }
+$(".up-tool").click(() => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
